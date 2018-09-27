caaf4aa820cb7bf64476b59f61d52fdab43b56e0
vampirate <vmprt@Daniels-MacBook-Pro-3.local>
2018-09-27 14:12:20 +1000

    updated the code

--- Main.cpp
+++ Main.cpp
@@ -5,22 +5,24 @@
 #include <string>
 using namespace cv;
 using namespace std;
-int main( int argc, char** argv )
-{
-    String imageName( "cat.jpg" ); // by default
-    if( argc > 1)
-    {
+int main(int argc, char** argv) {
+    String imageName("cat.jpg"); // Default image
+
+    if(argc > 1) {
         imageName = argv[1];
     }
+
     Mat image;
-    image = imread( imageName, IMREAD_COLOR ); // Read the file
-    if( image.empty() )                      // Check for invalid input
-    {
+    image = imread(imageName, IMREAD_COLOR); // Read the file
+
+    if(image.empty()) {
         cout <<  "Could not open or find the image" << std::endl ;
         return -1;
     }
-    namedWindow( "Display window", WINDOW_AUTOSIZE ); // Create a window for display.
-    imshow( "Display window", image );                // Show our image inside it.
+
+    namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
+    imshow("Display window", image);                // Show our image inside it.
     waitKey(0); // Wait for a keystroke in the window
+    
     return 0;
 }
