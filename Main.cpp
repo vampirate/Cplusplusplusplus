f4dd39a8244205ccbe28a63473b9cc740706e28e
vampirate <vmprt@Daniels-MacBook-Pro-3.local>
2018-09-27 11:28:24 +1000

    testtest

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World!";
-  std::cout << "Weow";
+  std::cout << "Hello World!" << std::endl;
+  std::cout << "Weow" << std::endl;
   int count;
   count = 0;
   for (count = 0; count < 10; count++) {
-    std::cout << "Weow";
+    std::cout << "Weow" << std::endl;
   }
 }
