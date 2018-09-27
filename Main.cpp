aa4233b6c36d623a7ea82306a401f5b1e8504cf3
vampirate <vmprt@Daniels-MacBook-Pro-3.local>
2018-09-27 10:21:35 +1000

    FIRST

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!";
+  std::cout << "Weow";
+  int count;
+  count = 0;
+  for (count = 0; count < 10; count++) {
+    std::cout << "Weow";
+  }
+}
